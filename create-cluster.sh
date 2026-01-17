8c2054efdc0d58d87c2aed749c8008ba9c0f77e0
tmp1git <tmp.use.tu+github@gmail.com>
2026-01-18 06:52:50 +0900

    Initial commit: Kubernetes + Jupyter + Selenium setup on WSL

---
+++ create-cluster.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 1. kind クラスタ作成
+kind create cluster --config kind-config.yaml
+
+# 2. Ingress Controller をインストール
+kubectl apply -f ingress-controller/deploy-ingress-nginx.yaml
+
+# 3. Ingress Controller が Ready になるまで待つ
+#kubectl wait --namespace ingress-nginx \
+#  --for=condition=ready pod \
+#  --selector=app.kubernetes.io/component=controller \
+#  --timeout=90s
+
